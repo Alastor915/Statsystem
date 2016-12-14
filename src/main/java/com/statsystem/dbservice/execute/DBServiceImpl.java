31fb66d0660cc8f36361801786931bb9e6d03ebc
HoustonVS <forhoustonvs@gmail.com>
2016-12-15 03:34:08 +0400

    dbvalidate

--- src/main/java/com/statsystem/dbservice/execute/DBServiceImpl.java
+++ src/main/java/com/statsystem/dbservice/execute/DBServiceImpl.java
@@ -25,7 +25,7 @@ import java.util.List;
 
 public class DBServiceImpl implements DBService {
     private static final String hibernate_show_sql = "true";
-    private static final String hibernate_hbm2ddl_auto = "create";
+    private static final String hibernate_hbm2ddl_auto = "validate";
 
     private final SessionFactory sessionFactory;
 
